514e341eccb329762975562b59ada78421d70906
Brian Lai <brianlai30@outlook.com>
2025-03-30 22:12:47 -0700

    Initial commit

---
+++ src/main/java/com/blai30/mixin/ScreenHandlerMixin.java
@@ -0,0 +1,38 @@
+package com.blai30.mixin;
+
+import net.minecraft.item.Items;
+import net.minecraft.screen.*;
+import net.minecraft.screen.slot.SlotActionType;
+import net.minecraft.server.network.ServerPlayerEntity;
+import net.minecraft.text.Text;
+import org.spongepowered.asm.mixin.Mixin;
+import org.spongepowered.asm.mixin.injection.At;
+import org.spongepowered.asm.mixin.injection.Inject;
+import org.spongepowered.asm.mixin.injection.callback.CallbackInfo;
+
+import java.util.Objects;
+
+@Mixin(ScreenHandler.class)
+public class ScreenHandlerMixin {
+    @Inject(method = "onSlotClick", at = @At("HEAD"), cancellable = true)
+    private void onSlotClick(int slotId, int button, SlotActionType actionType, net.minecraft.entity.player.PlayerEntity player, CallbackInfo ci) {
+        ScreenHandler handler = (ScreenHandler) (Object) this;
+        boolean isPlayerInventory = handler instanceof PlayerScreenHandler;
+        boolean isSurvival = !player.getAbilities().creativeMode;
+
+        if (isPlayerInventory && isSurvival && player instanceof ServerPlayerEntity && button == 1) {
+            if (actionType == SlotActionType.PICKUP && player.currentScreenHandler.getSlot(slotId).hasStack()) {
+                var stack = player.currentScreenHandler.getSlot(slotId).getStack();
+
+                if (stack.getItem() == Items.CRAFTING_TABLE) {
+                    Objects.requireNonNull(player.getServer()).execute(() -> {
+                        player.openHandledScreen(new SimpleNamedScreenHandlerFactory((syncId, playerInventory, playerEntity) -> {
+                            return new CraftingScreenHandler(syncId, playerInventory, ScreenHandlerContext.EMPTY);
+                        }, Text.translatable("container.crafting")));
+                    });
+                }
+            }
+            ci.cancel();
+        }
+    }
+}
